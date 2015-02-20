902e4a83fd36169d7fc7cffda35c7f3c6f205585
amm <amm@amm-VirtualBox.(none)>
2015-02-20 21:38:01 +0100

    bug fix

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <h2>Benvenuto su Computer Shop!</h2><br>
-In questo sito potrai efettuare acquisiti o provare ad inserire nuovi prodotti nel database
+Computer Shop e' un sito di eCommerce che permette agli utenti di acquistare pc, tablet o smartphone.<br><br>
+
+Strumenti con cui e' stato realizzato il sito:<br>
+- HTML e CSS<br>
+- Model View Controller<br>
+- MySQL e PHP<br>
+- Javascript e AJAX (presenti nel footer di ogni pagina)<br>
+- Gestione di piu' utenti (sito base, utente loggato, admin)<br>
+- Transazione (acquistare i prodotti contenuti nel carrello)<br><br>
+
+Strumenti che il sito NON utilizza<br>
+- Rewrite engine<br>
+- CSS responsive<br>
+- Admin con IMP Token<br>
+- Gestione immagini per id prodotto<br><br>
+
+Cliccando il pulsante qui presente e' possibile raggiungere il sito.<br>
+Buon proseguimento!<br><br>
+
 <form action="php/index.php" method="post">
         <input type="submit" name="login" value="Clicca qui per accedere">
 </form>
